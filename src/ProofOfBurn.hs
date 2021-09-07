d0d723d378748b62b71a047d908fc7adfa2fc231
Michal J. Gajda <mjgajda@migamake.com>
2021-09-07 05:30:47 +0200

    Fixed the confirmation script.

--- src/ProofOfBurn.hs
+++ src/ProofOfBurn.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase                 #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NamedFieldPuns             #-}
 {-# LANGUAGE NoImplicitPrelude          #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE RecordWildCards            #-}
@@ -36,14 +37,14 @@ import qualified Data.List.NonEmpty        as NonEmpty(toList)
 import           Plutus.Contract
 import qualified PlutusTx         as PlutusTx
 import           PlutusTx.Prelude hiding (Applicative (..))
-import           Ledger                    (Address, scriptAddress)
+import           Ledger                    (Address, Datum(..), scriptAddress)
 import qualified Ledger.Constraints        as Constraints
 import qualified Ledger.Typed.Scripts      as Scripts
 import           Ledger.Typed.Scripts.Validators(ValidatorType)
 import           Ledger.Value              (Value)
 import           Plutus.V1.Ledger.Crypto
 import           Plutus.V1.Ledger.Contexts
-import           Plutus.V1.Ledger.Tx (Tx, TxOutRef, TxOutTx(..), txData, txOutDatum)
+import           Plutus.V1.Ledger.Tx (Tx(..), TxOutRef, TxOutTx(..), txData, txOutDatum)
 import           Playground.Contract
 
 -- | Utxo datum holds either:
@@ -66,18 +67,11 @@ validateSpend _ _  _ = traceError "Expecting exactly one signatory."
 contractAddress :: Address
 contractAddress = Ledger.scriptAddress (Scripts.validatorScript burnerValidator)
 
+-- | Type level tag for the Burner script.
 data Burner
 instance Scripts.ValidatorTypes Burner where
-    type instance RedeemerType  Burner = MyRedeemer
-    type instance DatumType     Burner = MyDatum
-
-{-
-burnerInstance :: Scripts.ScriptInstance Burner
-burnerInstance = Scripts.validator @Burner
-    $$(PlutusTx.compile [|| validateSpend ||])
-    $$(PlutusTx.compile [|| wrap ||]) where
-        wrap = Scripts.wrapValidator @MyDatum @MyRedeemer
- -}
+    type instance RedeemerType  Burner = MyRedeemer -- ^ Argument given to redeem value, if possible (empty)
+    type instance DatumType     Burner = MyDatum    -- ^ Validator script argument
 
 -- | The script instance is the compiled validator (ready to go onto the chain)
 burnerValidator :: Scripts.TypedValidator Burner
@@ -137,24 +131,34 @@ redeem = do
     void $ submitTxConstraintsSpending burnerValidator unspentOutputs tx
 
 
--- | The "burned" confirmation endpoint
+-- | The "burned" confirmation endpoint.
 --
 --   Confirms that the value was burned with the given commitment.
-{-
+--   It returns total value locked with a given commitment.
 burned :: AsContractError e => ByteString -> Contract w Schema e Value
 burned aCommitment = do
-    unspentOutputs <- utxoAt contractAddress
+    unspentOutputs :: UtxoMap <- utxoAt contractAddress
     return $ totalValue (withCommitment `Map.filter` unspentOutputs)
-    {-let redeemer = MyRedeemer
-        tx       = collectFromScript unspentOutputs redeemer
-    void $ submitTxConstraintsSpending burnerInstance unspentOutputs tx -}
   where
-    totalValue         = mconcat . fmap (txOutValue . snd) . Map.toList
+    -- | Total value of Utxos in a `UtxoMap`.
+    totalValue        :: UtxoMap -> Value
+    totalValue         = mconcat . fmap (txOutValue . txOutTxOut . snd) . Map.toList
+    -- | Check if the given transaction output is commited to burn with the same hash as `aCommitment`.
     withCommitment    :: TxOutTx -> Bool
-    withCommitment txo = (fmap fromMyDatum $ PlutusTx.fromData $ txOutDatum $ txData $ txOutTxTx txo) == Just hash
-    hash               = flipCommitment $ sha3_256 aCommitment
- -}
+    withCommitment TxOutTx { txOutTxTx  = Tx    { txData         } -- Mapping of transaction DatumHashes to Datum.
+                           , txOutTxOut = TxOut { txOutDatumHash } -- DatumHash for the given transaction output.
+                           } = burnHash == Just commitmentHash
+      where
+        -- | Extract hash from the Utxo.
+        burnHash  :: Maybe ByteString
+        burnHash   = do
+          dh            <- txOutDatumHash
+          Datum d       <- dh `Map.lookup` txData -- lookup Datum from the hash
+          MyDatum aHash <- PlutusTx.fromData d -- Unpack from PlutusTx.Datum type.
+          return           aHash
+    commitmentHash = flipCommitment $ sha3_256 aCommitment
 
+-- | Script endpoints available for calling the contract.
 endpoints :: AsContractError e => Contract w Schema e ()
 endpoints = contract
 

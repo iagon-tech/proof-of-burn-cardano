110f93c36beab3fbf9abd86718d08d963a768333
Dmitry Krylov <dima@krylov.ws>
2021-10-03 21:01:21 +1000

    Prop.test for `lock` and `redeem`

--- test/PropTests.hs
+++ test/PropTests.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE NamedFieldPuns        #-}
 {-# LANGUAGE DeriveAnyClass        #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleContexts      #-}
@@ -17,6 +18,7 @@ module PropTests where
 
 
 import           Control.Lens                       hiding (elements)
+import           Control.Exception.Lens
 import           Control.Monad                      (void, when)
 import           Data.Default                       (Default (..))
 import           Data.Map                           (Map)
@@ -32,6 +34,9 @@ import           Ledger                             hiding (singleton)
 import           Ledger.Ada                         as Ada
 import           Ledger.Value
 
+-- import Control.Eff.Exception as Eff
+import Control.Monad.Freer.Error
+
 import           Test.QuickCheck
 import           Test.Tasty
 import           Test.Tasty.QuickCheck
@@ -39,14 +44,17 @@ import           Test.Tasty.QuickCheck
 import           ProofOfBurn
 
 
-data POBState = POBState
-    { _psVal :: !Integer
-    } deriving Show
+--data POBState = POBState
+--    { _psLocks :: !Map Wallet Integer
+--    } deriving Show
 
-makeLenses ''POBState
+--makeLenses ''POBState
+--
+
+-- TODO use `Value` (or even `Ada Value`) instead of `Integer`
 
 
-newtype POBModel = POBModel { _pobModel :: Map Wallet POBState }
+data POBModel = POBModel { _pobLocks :: Map Wallet Integer }
     deriving Show
 
 makeLenses ''POBModel
@@ -61,34 +69,46 @@ instance ContractModel POBModel where
 
 
     data ContractInstanceKey POBModel w s e where
-        POBKey :: Wallet -> ContractInstanceKey POBModel () ProofOfBurn.Schema Text
+        -- TODO So we can't use contract twice?? Well, ok, let's use current slot to make new intsance
+        POBKey :: Wallet -> Slot -> ContractInstanceKey POBModel () ProofOfBurn.Schema Text
 
     arbitraryAction _ = oneof $
         [ Lock   <$> genWallet <*> genWallet <*> genNonNeg -- TODO gen any number, even negative and zero
-        -- , Redeem <$> genWallet
+        , Redeem <$> genWallet
         ]
 
     initialState = POBModel Map.empty
 
     nextState (Lock wFrom wTo v) = do
-        -- do something
         withdraw wFrom (Ada.lovelaceValueOf v)
+        (pobLocks . at wTo) $~ (Just . maybe v (+v))
         wait 1
 
-    nextState (Redeem w) = do
-        -- do something
-        -- TODO catch exception from wrong `redeem` :-/
+    nextState (Redeem wTo) = do
+        v <- askContractState (Map.lookup wTo . _pobLocks) -- TODO change to lens
+        case v of
+            Nothing -> return ()
+            Just (vv::Integer) -> do
+                deposit wTo (Ada.lovelaceValueOf vv)
+                (pobLocks . at wTo) $= Nothing -- TODO change to deletion via lens
         wait 1
 
 
-    perform h _ cmd = case cmd of
-        (Lock   wFrom wTo v) -> callEndpoint @"lock"   (h $ POBKey wFrom) (pubKeyHash $ walletPubKey wTo, Ada.lovelaceValueOf v) >> delay 1
-        -- TODO test redeem from SOME wallets (+ to unit tests)
-        (Redeem       wTo)   -> callEndpoint @"redeem" (h $ POBKey wTo)   ()                                                     >> delay 1
+    perform h ms cmd = do
+        let slot = ms ^. Plutus.Contract.Test.ContractModel.currentSlot
+        case cmd of
+            (Lock   wFrom wTo v) -> callEndpoint @"lock"   (h $ POBKey wFrom slot) (pubKeyHash $ walletPubKey wTo, Ada.lovelaceValueOf v) >> delay 1
+            (Redeem       wTo)   -> 
+                -- TODO here it is need to catch and process error from endpoint
+                callEndpoint @"redeem" (h $ POBKey wTo slot)   () >> delay 1
+                -- catchError (callEndpoint @"redeem" (h $ POBKey wTo slot)   ()) (\(_:: Error EmulatorRuntimeError ) -> undefined)
+                -- catching _ContractError (callEndpoint @"redeem" (h $ POBKey wTo slot)   ()) (\_ -> undefined)
+                -- >> delay 1
 
-    -- TODO
     precondition s (Lock wFrom wTo v) = True
-    precondition s (Redeem     wTo)   = True
+    precondition s (Redeem     wTo)   =
+        -- TODO remove this check: script will work with empty redeem (but it is need to catch error)
+        isJust (s ^. contractState . pobLocks . at wTo)
 
 deriving instance Eq (ContractInstanceKey POBModel w s e)
 deriving instance Show (ContractInstanceKey POBModel w s e)
@@ -97,7 +117,7 @@ delay :: Int -> EmulatorTrace ()
 delay = void . waitNSlots . fromIntegral
 
 wallets :: [Wallet]
-wallets = [w1, w2, w3, w4]
+wallets = [w1, w2, w3] -- TODO add more wallets
 
 genWallet :: Gen Wallet
 genWallet = elements wallets
@@ -108,16 +128,21 @@ genNonNeg = (+1) <$> (getNonNegative <$> arbitrary)
 
 instanceSpec :: [ContractInstanceSpec POBModel]
 instanceSpec =
-    [ContractInstanceSpec (POBKey w) w ProofOfBurn.endpoints | w <- wallets]
-
+    [ContractInstanceSpec (POBKey w s) w ProofOfBurn.endpoints | w <- wallets
+                                                               , s <- map Slot [0..100] ] -- TODO it seems very weird to make contract for every `lock`...
+                                                                                          --      another way is to precreate some contracts for `lock` only and use counter
+                                                                                          --      in model, not `currentSlot`
+                                                                                          --
+                                                                                          -- Another problem is that instances make very slow
+                                                                                          --
 -- * --------------------------------------------------------------------------
 
 tests :: TestTree
-tests = testProperty "PoB" prop_POB
+tests = testProperty "prop tests" prop_POB
 
 
 prop_POB :: Actions POBModel -> Property
-prop_POB = withMaxSuccess 1 . propRunActionsWithOptions
+prop_POB = withMaxSuccess 100 . propRunActionsWithOptions
     (defaultCheckOptions & emulatorConfig .~ EmulatorConfig (Left initDistr) def def)
     instanceSpec
     (const $ pure True)

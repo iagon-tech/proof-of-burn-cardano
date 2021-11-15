6cbd2b2f2c5b1eeeb634e93755d3e587758165e5
Julian Ospald <hasufell@posteo.de>
2021-11-15 13:32:47 +0100

    Require password argument for start-testnet-pab.sh

--- start-testnet-pab.sh
+++ start-testnet-pab.sh
@@ -4,12 +4,13 @@ set -eux
 
 # bin=$(cabal list-bin exe:plutus-pab-examples)
 bin=$(cabal list-bin exe:plutus-burner-pab)
+pass=$1
 
 "${bin}" \
     --config testnet/pab-config.yml migrate
 
 exec "${bin}" \
     --config testnet/pab-config.yml webserver \
-    --passphrase pab123456789
+    --passphrase "${pass}"
 
 
